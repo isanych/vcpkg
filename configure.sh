8806c417054cd0028f1f48ebb52b4b51ef1f00c6
Igor Kostenko <ikostenko@perforce.com>
2019-04-10 15:40:50 +0100

    protoc may not have exec flag

--- configure.sh
+++ configure.sh
@@ -5,3 +5,4 @@ export CC=`which gcc`
 export CXX=`which g++`
 "$vcpkgRootDir/bootstrap-vcpkg.sh" -useSystemBinaries
 "$vcpkgRootDir/vcpkg" install qt5-base grpc highfive boost rapidjson cryptopp
+chmod +x "$vcpkgRootDir/installed/x64-linux/tools/protobuf/protoc-*"
