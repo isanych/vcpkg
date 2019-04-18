758782fee94231d58cb5f965073da11d82dbc09e
Igor Kostenko <ikostenko@perforce.com>
2019-04-18 09:50:59 +0100

    dynamic linux

--- configure.sh
+++ configure.sh
@@ -6,3 +6,6 @@ export CXX=`which g++`
 "$vcpkgRootDir/bootstrap-vcpkg.sh" -useSystemBinaries
 "$vcpkgRootDir/vcpkg" install qt5-base grpc highfive boost rapidjson cryptopp
 chmod +x $vcpkgRootDir/installed/x64-linux/tools/protobuf/protoc-*
+ln -s ../tools/qt5/moc $vcpkgRootDir/installed/x64-linux/bin/moc
+ln -s ../tools/qt5/qmake $vcpkgRootDir/installed/x64-linux/bin/qmake
+ln -s ../tools/protobuf/protoc $vcpkgRootDir/installed/x64-linux/bin/protoc
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -235,7 +235,7 @@ buildDir="$vcpkgRootDir/toolsrc/build.rel"
 rm -rf "$buildDir"
 mkdir -p "$buildDir"
 
-(cd "$buildDir" && CXX=$CXX "$cmakeExe" .. -DCMAKE_BUILD_TYPE=Release -G "Ninja" "-DCMAKE_MAKE_PROGRAM=$ninjaExe" "-DDEFINE_DISABLE_METRICS=$vcpkgDisableMetrics") || exit 1
+(cd "$buildDir" && CXX=$CXX "$cmakeExe" .. -DCMAKE_BUILD_TYPE=Debug -G "Ninja" "-DCMAKE_MAKE_PROGRAM=$ninjaExe" "-DDEFINE_DISABLE_METRICS=$vcpkgDisableMetrics") || exit 1
 (cd "$buildDir" && "$cmakeExe" --build .) || exit 1
 
 rm -rf "$vcpkgRootDir/vcpkg"
