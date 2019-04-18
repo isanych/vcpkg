10d213422fd8a94caa896968098130abb2d4903c
Igor Kostenko <ikostenko@perforce.com>
2019-04-18 14:15:42 +0100

    attempt to build qt dynamically when everythine else statically

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -1,5 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
+if(${PORT} MATCHES "^qt5")
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
