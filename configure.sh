5888c4016b17b19108d273984d83a43752f228e9
Igor Kostenko <ikostenko@perforce.com>
2019-04-30 11:09:00 +0100

    added icu

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 export CC=`which gcc`
 export CXX=`which g++`
 "$vcpkgRootDir/bootstrap-vcpkg.sh" -useSystemBinaries
-"$vcpkgRootDir/vcpkg" install qt5-base grpc highfive boost rapidjson cryptopp qt5-script qt5-xmlpatterns
+"$vcpkgRootDir/vcpkg" install icu grpc highfive boost rapidjson cryptopp qt5-base qt5-script qt5-xmlpatterns
 chmod +x $vcpkgRootDir/installed/x64-linux/tools/protobuf/protoc-*
 ln -s ../tools/qt5/moc $vcpkgRootDir/installed/x64-linux/bin/moc
 ln -s ../tools/qt5/qmake $vcpkgRootDir/installed/x64-linux/bin/qmake
