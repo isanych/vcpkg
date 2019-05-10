943c9ec1fd1882f5d8447e4acbcba977d665527c
Igor Kostenko <ikostenko@perforce.com>
2019-05-10 12:38:49 +0100

    build qt5-xmlpatterns dynamically on windows

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -1,4 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-
+if(PORT STREQUAL "qt5-xmlpatterns")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
