2c533aa30e00196dc6faf611d97b73eccd8af647
Igor Kostenko <ikostenko@perforce.com>
2019-05-31 16:30:10 +0100

    windows tar content consistent with linux

--- rebuild.cmd
+++ rebuild.cmd
@@ -2,10 +2,14 @@ cd "%~dp0"
 rd /q/s buildtrees downloads installed packages
 copy triplets\x64-windows-dynamic.cmake triplets\x64-windows.cmake
 cmd /c configure
-tar xf ..\vcpkg-latest-windows-x64-add.tgz
-tar czf ..\vcpkg-latest-windows-x64-vs2019-dynamic.tgz installed/x64-windows scripts triplets .vcpkg-root
+cd ..
+tar xf vcpkg-latest-windows-x64-add.tgz
+tar czf vcpkg-latest-windows-x64-vs2019-dynamic.tgz vcpkg/installed/x64-windows vcpkg/scripts vcpkg/triplets vcpkg/.vcpkg-root
+cd vcpkg
 rd /q/s buildtrees downloads installed packages
 copy triplets\x64-windows-default.cmake triplets\x64-windows.cmake
 cmd /c configure
-tar xf ..\vcpkg-latest-windows-x64-add.tgz
-tar czf ..\vcpkg-latest-windows-x64-vs2019-static.tgz installed/x64-windows scripts triplets .vcpkg-root
+cd ..
+tar xf vcpkg-latest-windows-x64-add.tgz
+tar czf vcpkg-latest-windows-x64-vs2019-static.tgz vcpkg/installed/x64-windows vcpkg/scripts vcpkg/triplets vcpkg/.vcpkg-root
+cd vcpkg
