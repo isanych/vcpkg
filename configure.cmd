db204749f0de3f11d957840329a175b27baa491f
Igor Kostenko <ikostenko@perforce.com>
2019-06-03 09:52:51 +0100

    detect build tools

--- configure.cmd
+++ configure.cmd
@@ -1,4 +1,4 @@
-for /f "delims=" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -prerelease -property installationPath') do set p=%%i
+for /f "delims=" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -all -latest -products * -prerelease -property installationPath') do set p=%%i
 call "%p%\VC\Auxiliary\Build\vcvars64.bat"
 call "%~dp0bootstrap-vcpkg"
 "%~dp0vcpkg" install qt5-base grpc highfive boost rapidjson cryptopp qt5-script qt5-xmlpatterns
