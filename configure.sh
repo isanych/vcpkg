5c907fa437c86f9775605e25a0749178ab20b83a
Igor Kostenko <ikostenko@perforce.com>
2019-06-17 17:41:40 +0100

    set exec flags

--- configure.sh
+++ configure.sh
@@ -4,8 +4,11 @@ vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 export CC=`which gcc`
 export CXX=`which g++`
 "$vcpkgRootDir/bootstrap-vcpkg.sh" -useSystemBinaries
-"$vcpkgRootDir/vcpkg" install icu grpc highfive boost rapidjson cryptopp qt5-base qt5-script qt5-xmlpatterns
-chmod +x $vcpkgRootDir/installed/x64-linux/tools/protobuf/protoc-*
+"$vcpkgRootDir/vcpkg" install icu grpc highfive boost rapidjson cryptopp qt5-base
+/opt/vcpkg# ls -la ./installed/x64-linux/tools/qt5
+chmod +x $vcpkgRootDir/installed/x64-linux/tools/protobuf/protoc-* $vcpkgRootDir/installed/x64-linux/tools/qt5/*
+chmod -x $vcpkgRootDir/installed/x64-linux/tools/qt5/*.conf
 ln -s ../tools/qt5/moc $vcpkgRootDir/installed/x64-linux/bin/moc
 ln -s ../tools/qt5/qmake $vcpkgRootDir/installed/x64-linux/bin/qmake
 ln -s ../tools/protobuf/protoc $vcpkgRootDir/installed/x64-linux/bin/protoc
+"$vcpkgRootDir/vcpkg" install qt5-script qt5-xmlpatterns
