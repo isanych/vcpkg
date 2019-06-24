ce50b2598fa77048bba219799378cea74e061c90
Igor Kostenko <ikostenko@perforce.com>
2019-06-24 15:31:20 +0100

    explicit dependecies, vs2017 compiler

--- configure.cmd
+++ configure.cmd
@@ -1,6 +1,6 @@
 for /f "delims=" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -all -latest -products * -prerelease -property installationPath') do set p=%%i
-call "%p%\VC\Auxiliary\Build\vcvars64.bat"
+call "%p%\VC\Auxiliary\Build\vcvars.bat" x64 -vcvars_ver=14.16.27023
 if not exist "%~dp0vcpkg.exe" call "%~dp0bootstrap-vcpkg"
-"%~dp0vcpkg" install grpc highfive boost rapidjson cryptopp
+"%~dp0vcpkg" install protobuf grpc hdf5 highfive boost rapidjson cryptopp
 rem "%~dp0vcpkg" install bzip2 double-conversion freetype harfbuzz libjpeg-turbo liblzma libpng openssl pcre2 sqlite3 zlib
 copy "%~dp0installed\x64-windows\tools\protobuf\*.exe" "%~dp0installed\x64-windows\bin"
