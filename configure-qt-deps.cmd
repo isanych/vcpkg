6974e56c1d559cc7803a68f6b5a7c1434686815b
Igor Kostenko <ikostenko@perforce.com>
2019-06-29 16:17:30 +0100

    qt with lto

--- configure-qt-deps.cmd
+++ configure-qt-deps.cmd
@@ -1,2 +1,2 @@
 if not exist "%~dp0vcpkg.exe" call "%~dp0bootstrap-vcpkg"
-"%~dp0vcpkg" install --triplet bzip2 double-conversion freetype harfbuzz libjpeg-turbo liblzma libpng openssl pcre2 sqlite3 zlib fontconfig
+"%~dp0vcpkg" install --triplet x64-windows-qt zlib bzip2 double-conversion icu libjpeg-turbo liblzma libpng openssl pcre2 sqlite3 freetype harfbuzz fontconfig
--- configure-qt-deps.sh
+++ configure-qt-deps.sh
@@ -4,4 +4,4 @@ vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 export CC=`which gcc`
 export CXX=`which g++`
 [[ -f "$vcpkgRootDir/vcpkg" ]] || "$vcpkgRootDir/bootstrap-vcpkg.sh" -useSystemBinaries
-"$vcpkgRootDir/vcpkg" --triplet x64-linux-qt install icu bzip2 double-conversion freetype harfbuzz libjpeg-turbo liblzma libpng openssl pcre2 sqlite3 zlib fontconfig
+"$vcpkgRootDir/vcpkg" --triplet x64-linux-qt install zlib bzip2 double-conversion icu libjpeg-turbo liblzma libpng openssl pcre2 sqlite3 freetype harfbuzz fontconfig
