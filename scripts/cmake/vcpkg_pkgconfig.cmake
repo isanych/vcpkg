061de59992583944deae178779da4a1c9d89084c
Igor Kostenko <ikostenko@perforce.com>
2019-07-02 13:36:52 +0100

    cleanup

--- scripts/cmake/vcpkg_pkgconfig.cmake
+++ scripts/cmake/vcpkg_pkgconfig.cmake
@@ -6,7 +6,7 @@
 ## ```cmake
 ## vcpkg_pkgconfig(
 ##     [NAME <${PORT}>]
-##     [NO_REMOVE]
+##     [REMOVE]
 ##     [COMMON <-l${PORT}>...]
 ##     [RELEASE ...]
 ##     [DEBUG ...]
@@ -22,9 +22,6 @@
 ##
 ## ### REMOVE
 ## Remove pkgconfig directory before creating pc files.
-##
-## ### DISABLE_PARALLEL_CONFIGURE
-## Disables running the CMake configure step in parallel.
 ##
 ## ### COMMON
 ## Common libraries for `Libs:` section
@@ -52,17 +49,17 @@ function(vcpkg_pkgconfig)
     if(CMAKE_HOST_WIN32)
         return()
     endif()
-    cmake_parse_arguments(_pc "NO_REMOVE" "NAME" "COMMON;RELEASE;DEBUG;REQUIRES;REQUIRES_RELEASE;REQUIRES_DEBUG" ${ARGN})
-	set(PORT_LIBS_RELEASE ${_pc_COMMON} ${_pc_RELEASE})
-	set(PORT_LIBS_DEBUG ${_pc_COMMON} ${_pc_DEBUG})
+    cmake_parse_arguments(_pc "REMOVE" "NAME" "COMMON;RELEASE;DEBUG;REQUIRES;REQUIRES_RELEASE;REQUIRES_DEBUG" ${ARGN})
+    set(PORT_LIBS_RELEASE ${_pc_COMMON} ${_pc_RELEASE})
+    set(PORT_LIBS_DEBUG ${_pc_COMMON} ${_pc_DEBUG})
     if(NOT PORT_LIBS_RELEASE)
         set(PORT_LIBS_RELEASE "-l${PORT}")
     endif()
     if(NOT PORT_LIBS_DEBUG)
         set(PORT_LIBS_DEBUG "-l${PORT}")
     endif()
-	set(PORT_REQUIRES_RELEASE ${_pc_REQUIRES} ${_pc_REQUIRES_RELEASE})
-	set(PORT_REQUIRES_DEBUG ${_pc_REQUIRES} ${_pc_REQUIRES_DEBUG})
+    set(PORT_REQUIRES_RELEASE ${_pc_REQUIRES} ${_pc_REQUIRES_RELEASE})
+    set(PORT_REQUIRES_DEBUG ${_pc_REQUIRES} ${_pc_REQUIRES_DEBUG})
     if(NOT PORT_REQUIRES_RELEASE)
         set(PORT_REQUIRES_RELEASE ${PORT_DEPENDENCIES})
     endif()
@@ -77,13 +74,13 @@ function(vcpkg_pkgconfig)
     list(JOIN PORT_REQUIRES_RELEASE ", " PORT_REQUIRES_RELEASE)
     list(JOIN PORT_REQUIRES_DEBUG ", " PORT_REQUIRES_DEBUG)
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-	    if(_pc_REMOVE)
+        if(_pc_REMOVE)
             file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
         endif()
         configure_file("${VCPKG_ROOT_DIR}/scripts/templates/release.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/${_pc_NAME}.pc" @ONLY)
     endif()
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-	    if(_pc_REMOVE)
+        if(_pc_REMOVE)
             file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
         endif()
         configure_file("${VCPKG_ROOT_DIR}/scripts/templates/debug.pc.in" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/${_pc_NAME}.pc" @ONLY)
