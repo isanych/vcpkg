0023131cdd41df4279034e83c27449f5573280d2
Igor Kostenko <ikostenko@perforce.com>
2019-07-30 16:36:37 +0100

    dynamic hdf5

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -1,10 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-if(PORT MATCHES "^qt5.*")
+if(PORT MATCHES "^qt5.*" OR PORT STREQUAL hdf5)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
-  set(VCPKG_LIBRARY_LINKAGE static)
+    set(VCPKG_LIBRARY_LINKAGE static)
 endif()
