fac96cf8b4c67a40772da574b7b808531c67bcb9
Igor Kostenko <ikostenko@perforce.com>
2019-08-01 17:10:41 +0100

    everything dynamic and lto

--- triplets/x86-windows-default.cmake
+++
@@ -1,7 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x86)
-set(VCPKG_CRT_LINKAGE dynamic)
-if(PORT MATCHES "^qt5.*" OR PORT STREQUAL hdf5)
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-else()
-    set(VCPKG_LIBRARY_LINKAGE static)
-endif()
