d3a1b1b4885a6031c7d1ee0a952e3cb8859fbfb4
Igor Kostenko <ikostenko@perforce.com>
2019-10-04 11:26:21 +0100

    qt build and postinstall fixes

--- configure.sh
+++ configure.sh
@@ -6,6 +6,6 @@ export CC=`which gcc`
 export CXX=`which g++`
 [[ ! -d /deploy/vcpkg/downloads || -e downloads ]] || ln -s /deploy/vcpkg/downloads
 [[ -f vcpkg ]] || ./bootstrap-vcpkg.sh -useSystemBinaries
-LD_LIBRARY_PATH="$vcpkgRootDir/installed/x64-linux/lib:$vcpkgRootDir/installed/x64-linux/debug/lib" http_proxy=http://proxy.prqa.co.uk:80 ./vcpkg install zlib bzip2 liblzma double-conversion icu libjpeg-turbo libpng openssl pcre pcre2 sqlite3 freetype harfbuzz fontconfig glib protobuf grpc hdf5 highfive boost rapidjson cryptopp xerces-c xalan-c qt5-base
+LD_LIBRARY_PATH="$vcpkgRootDir/installed/x64-linux/lib:$vcpkgRootDir/installed/x64-linux/debug/lib" http_proxy=http://proxy.prqa.co.uk:80 ./vcpkg install zlib bzip2 liblzma double-conversion icu libjpeg-turbo libpng openssl pcre pcre2 sqlite3 freetype harfbuzz fontconfig protobuf grpc hdf5 highfive boost rapidjson cryptopp xerces-c xalan-c qt5-base qt-script
 cd installed/x64-linux
 ../../postinstall.py
