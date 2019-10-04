ce1987fdf02f43727d2e0a0e956a20e7f35dbba2
Igor Kostenko <ikostenko@perforce.com>
2019-10-04 17:35:03 +0100

    [qtwebengine] standart qt portfile

--- ports/qt5-webengine/portfile.cmake
+++ ports/qt5-webengine/portfile.cmake
@@ -1,5 +1,2 @@
-include(vcpkg_common_functions)
-
-include(${CURRENT_INSTALLED_DIR}/share/qt5modularscripts/qt_modular_library.cmake)
-
-qt_modular_library(qtwebengine 5b500ec6653aa6ed70e7826fe394f95c7932eaea5b1b48f6342a6f18294f75e4f954959fa2f42de0685097679389245d2bc80454e8eea202fa18a326d6d5a9a5)
+include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
+qt_submodule_installation()
