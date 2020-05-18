5955fa04455ab495ce21e61ec873701ddeb144ef
Igor Kostenko <isanych@gmail.com>
2020-05-18 15:58:39 +0100

    [freeglut] cmake fixup

--- ports/freeglut/portfile.cmake
+++ ports/freeglut/portfile.cmake
@@ -45,6 +45,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/FreeGLUT)
 
 # Rename static lib (otherwise it's incompatible with FindGLUT.cmake)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
