fd2c1b07227bd0f59b369314513181eac58df291
Igor Kostenko <isanych@gmail.com>
2020-06-15 17:13:44 +0100

    missing pcre on windows

--- configure.cmd
+++ configure.cmd
@@ -2,7 +2,7 @@ if [%x%] == [] set x=x64
 if not exist "%~dp0vcpkg.exe" call "%~dp0bootstrap-vcpkg" -disableMetrics
 set VCPKG_DEFAULT_TRIPLET=%x%-windows
 set PATH=%~dp0installed\%VCPKG_DEFAULT_TRIPLET%\lib;%~dp0installed\%VCPKG_DEFAULT_TRIPLET%\debug\lib;%PATH%
-"%~dp0vcpkg" install icu qt5-base qt5-script qt5-xmlpatterns qt5-webengine
+"%~dp0vcpkg" install pcre icu qt5-base qt5-script qt5-xmlpatterns qt5-webengine
 if %errorlevel% neq 0 exit /b %errorlevel%
 "%~dp0vcpkg" install protobuf hdf5 boost rapidjson cryptopp xerces-c xalan-c grpc
 if %errorlevel% neq 0 exit /b %errorlevel%
