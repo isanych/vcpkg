e5b5b808dc2737e4fd7be07d2c9639185105008f
Igor Kostenko <isanych@gmail.com>
2020-10-07 12:58:32 +0100

    fix add on windows

--- configure.cmd
+++ configure.cmd
@@ -7,7 +7,9 @@ set v="%~dp0vcpkg" install  --feature-flags=-compilertracking --editable
 if %errorlevel% neq 0 exit /b %errorlevel%
 %v% protobuf hdf5 boost rapidjson cryptopp xerces-c xalan-c grpc mimalloc[override]
 if %errorlevel% neq 0 exit /b %errorlevel%
+cd "%~dp0installed\%VCPKG_DEFAULT_TRIPLET%"
 copy "%~dp0postinstall.py" "%~dp0installed\%VCPKG_DEFAULT_TRIPLET%\"
 curl -Ss http://mist.prqa.co.uk/igor_kostenko/vcpkg-add/-/archive/windows%x32%/vcpkg-add-windows%x32%.tar.gz | tar xzf - --strip-components=1
+del .gitignore
 tar czf "%~dp0..\vcpkg-2020-windows-%x%-vs2019.tgz" -C "%~dp0.." vcpkg/installed/%VCPKG_DEFAULT_TRIPLET% vcpkg/scripts vcpkg/triplets/%VCPKG_DEFAULT_TRIPLET%.cmake vcpkg/.vcpkg-root
 curl -Ss -u build:buildpass --upload-file "%~dp0..\vcpkg-2020-windows-%x%-vs2019.tgz" http://nexus/repository/raw/vcpkg/vcpkg-2020-windows-%x%-vs2019.tgz
