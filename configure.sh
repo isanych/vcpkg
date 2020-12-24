ccb97c796e71d003054af2bd8bfe1ac1dcb7a6f9
Igor Kostenko <isanych@gmail.com>
2020-12-24 00:26:54 +0000

    configurable triplet on linux

--- configure.sh
+++ configure.sh
@@ -5,6 +5,7 @@ vcpkgRootDir=`pwd`
 export CC=`which gcc`
 export CXX=`which g++`
 unset SITE_CONFIG 
+[[ -n "${VCPKG_TRIPLET}" ]] || export VCPKG_TRIPLET=x64-linux
 [[ ! -d /deploy/vcpkg/downloads || -e downloads ]] || ln -s /deploy/vcpkg/downloads
 [[ -f vcpkg ]] || ./bootstrap-vcpkg.sh -useSystemBinaries
 if [[ "x${VCPKG_BOOST_STATIC}" = "xtrue" ]]; then
@@ -13,29 +14,29 @@ if [[ "x${VCPKG_BOOST_STATIC}" = "xtrue" ]]; then
 else
   : ${VCPKG_SUFFIX:=-dynamic}
 fi
-export LD_LIBRARY_PATH="$vcpkgRootDir/installed/x64-linux/lib:$vcpkgRootDir/installed/x64-linux/debug/lib"
-export PKG_CONFIG_PATH="$vcpkgRootDir/installed/x64-linux/lib/pkgconfig:$vcpkgRootDir/installed/x64-linux/debug/lib/pkgconfig"
+export LD_LIBRARY_PATH="$vcpkgRootDir/installed/${VCPKG_TRIPLET}/lib:$vcpkgRootDir/installed/${VCPKG_TRIPLET}/debug/lib"
+export PKG_CONFIG_PATH="$vcpkgRootDir/installed/${VCPKG_TRIPLET}/lib/pkgconfig:$vcpkgRootDir/installed/${VCPKG_TRIPLET}/debug/lib/pkgconfig"
 [[ ! -e /usr/lib64/libssl.so.1.1 ]] || export LD_LIBRARY_PATH="/usr/lib64:$LD_LIBRARY_PATH"
 if [[ "${VCPKG_BASE}" = centos7 ]]; then
   rm -rf /usr/local/include /usr/local/lib /usr/local/lib64
-  ln -s $vcpkgRootDir/installed/x64-linux/include /usr/local/include
-  ln -s $vcpkgRootDir/installed/x64-linux/lib /usr/local/lib
-  ln -s $vcpkgRootDir/installed/x64-linux/lib /usr/local/lib64
+  ln -s $vcpkgRootDir/installed/${VCPKG_TRIPLET}/include /usr/local/include
+  ln -s $vcpkgRootDir/installed/${VCPKG_TRIPLET}/lib /usr/local/lib
+  ln -s $vcpkgRootDir/installed/${VCPKG_TRIPLET}/lib /usr/local/lib64
 fi
 v="$vcpkgRootDir/vcpkg install --feature-flags=-compilertracking --editable"
 $v zstd
-cd installed/x64-linux/debug/lib
+cd installed/${VCPKG_TRIPLET}/debug/lib
 if [[ ! -f libzstd.so.1 ]]; then
   ln -s libzstdd.so.1 libzstd.so.1
   ln -s libzstdd.so libzstd.so
 fi
 cd $vcpkgRootDir
 $v glib libjpeg-turbo
-$v icu qt5-base qt5-script
+$v icu qt5-base
 [[ ! "${VCPKG_BASE}" = opensuse ]] || VCPKG_SKIP_EXTRA=1
 if [[ -z "${VCPKG_SKIP_EXTRA}" ]]; then
   $v libwebp
-  cd installed/x64-linux/debug/lib
+  cd installed/${VCPKG_TRIPLET}/debug/lib
   if [[ ! -f libwebpdecoder.so ]]; then
     ln -s libwebpdecoderd.so libwebpdecoder.so
     ln -s libwebpdecoderd.so.1.1.0 libwebpdecoder.so.1.1.0
@@ -51,11 +52,11 @@ if [[ -z "${VCPKG_SKIP_EXTRA}" ]]; then
     ln -s libwebpmuxd.so.3.5.0 libwebpmux.so.3.5.0
   fi
   cd $vcpkgRootDir
-  rm -f installed/x64-linux/lib/pkgconfig/freetype2.pc installed/x64-linux/debug/lib/pkgconfig/freetype2.pc
-  $v qt5-xmlpatterns qt5-webengine
+  rm -f installed/${VCPKG_TRIPLET}/lib/pkgconfig/freetype2.pc installed/${VCPKG_TRIPLET}/debug/lib/pkgconfig/freetype2.pc
+  $v qt5-script qt5-xmlpatterns qt5-webengine
 fi
 $v protobuf grpc hdf5 boost rapidjson cryptopp xerces-c xalan-c mimalloc[override]
-cd installed/x64-linux
+cd installed/${VCPKG_TRIPLET}
 chmod 777 tools/protobuf/*
 cd lib
 ln -sf libmimalloc.so.1.6 libmimalloc.so
@@ -73,4 +74,4 @@ if [[ -e $r ]]; then
   cp $r/rlm_nossl.a lib/
 fi
 ../../postinstall.py
-[[ -z "${VCPKG_BASE}" || ! -d /deploy/vcpkg ]] || LD_LIBRARY_PATH= tar cJf /deploy/vcpkg/vcpkg-2020-${VCPKG_BASE}-x64-gcc10${VCPKG_SUFFIX}.txz -C "$vcpkgRootDir/.." vcpkg/installed/x64-linux vcpkg/scripts vcpkg/triplets/x64-linux.cmake vcpkg/.vcpkg-root
+[[ -z "${VCPKG_BASE}" || ! -d /deploy/vcpkg ]] || LD_LIBRARY_PATH= tar cJf /deploy/vcpkg/vcpkg-2020-${VCPKG_BASE}-x64-gcc10${VCPKG_SUFFIX}.txz -C "$vcpkgRootDir/.." vcpkg/installed/${VCPKG_TRIPLET} vcpkg/scripts vcpkg/triplets/${VCPKG_TRIPLET}.cmake vcpkg/.vcpkg-root
