6c11dfb424a6591fa95ed9906ed4b5a6198d4a9c
Igor Kostenko <isanych@gmail.com>
2021-03-15 19:41:57 +0000

    echo on

--- configure.cmd
+++ configure.cmd
@@ -10,6 +10,7 @@ if [%x%] == [x64] %v% qt5-webengine
 if %errorlevel% neq 0 exit /b %errorlevel%
 %v% protobuf hdf5 boost rapidjson cryptopp xerces-c xalan-c grpc mimalloc[override] quazip
 if %errorlevel% neq 0 exit /b %errorlevel%
+echo on
 cd "%~dp0installed\%VCPKG_DEFAULT_TRIPLET%"
 copy "%~dp0postinstall.py" "%~dp0installed\%VCPKG_DEFAULT_TRIPLET%\"
 curl -Ss http://mist.prqa.co.uk/igor_kostenko/vcpkg-add/-/archive/windows%x32%/vcpkg-add-windows%x32%.tar.gz | tar xzf - --strip-components=1
