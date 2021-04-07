af6484f964eecfcc66306fb4d529916c9069feb4
Igor Kostenko <isanych@gmail.com>
2021-04-07 12:05:05 +0100

    [quazip] version 1.1

--- ports/quazip/portfile.cmake
+++ ports/quazip/portfile.cmake
@@ -1,10 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stachenov/quazip
-    REF v0.9.1
-    SHA512 db31f3c7e3d7e95c25090ceb8379643e0b49ed69ece009dd015bee120b2b60f42e73408f580caed3138fa19ca64dcd23a05f16435abb54e2b8df21105c7b42c0
-    PATCHES
-        vcpkg_remove_extra_static.patch
+    REF v1.1
+    SHA512 418516759e993c2e5636422c6a14e2caf95f836698b91d2188df5ef9b97879ee326255273793fc802325e14f378cbe2baad7e6ec2e1732e19bf238f70891f22c
 )
 
 vcpkg_configure_cmake(
@@ -13,7 +11,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/QuaZip5/)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/QuaZip-Qt5-1.1/ TARGET_PATH share/QuaZip-Qt5)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
