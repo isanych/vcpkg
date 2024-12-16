eac1d003d2481871d5a5a8e179d74dae2e66c02b
Igor Kostenko <ikostenko@perforce.com>
2024-12-16 20:51:43 +0000

    cleanup

--- archive.cmd
+++ archive.cmd
@@ -1,7 +1,5 @@
 if [%VCPKG_BRANCH%] == [] set VCPKG_BRANCH=2025
-if [%x%] == [] set x=x64
-if [%VCPKG_DEFAULT_TRIPLET%] == [] set VCPKG_DEFAULT_TRIPLET=%x%-windows
 if exist "%~dp0..\vcpkg\installed" (
-  tar czf "%~dp0..\vcpkg-%VCPKG_BRANCH%-windows-%x%-vs2022.tgz" -C "%~dp0.." vcpkg/installed/%VCPKG_DEFAULT_TRIPLET% vcpkg/scripts vcpkg/triplets/%VCPKG_DEFAULT_TRIPLET%.cmake vcpkg/.vcpkg-root
-  if not [%VCPKG_UPLOAD%] == [] curl -Ss %VCPKG_UPLOAD_CRED% --upload-file "%~dp0..\vcpkg-%VCPKG_BRANCH%-windows-%x%-vs2022.tgz" %VCPKG_UPLOAD%/vcpkg-%VCPKG_BRANCH%-windows-%x%-vs2022.tgz
+  tar czf "%~dp0..\vcpkg-%VCPKG_BRANCH%-windows.tgz" -C "%~dp0.." vcpkg/installed/x*-windows vcpkg/scripts vcpkg/triplets/x*-windows.cmake vcpkg/.vcpkg-root
+  if not [%VCPKG_UPLOAD%] == [] curl -Ss %VCPKG_UPLOAD_CRED% --upload-file "%~dp0..\vcpkg-%VCPKG_BRANCH%-windows.tgz" %VCPKG_UPLOAD%/vcpkg-%VCPKG_BRANCH%-windows.tgz
 )
