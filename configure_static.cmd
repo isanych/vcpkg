a8420a0a67c4c1a49ffde700c5c1b6129d7303c3
Igor Kostenko <ikostenko@perforce.com>
2025-01-22 08:00:05 +0000

    yices static

--- configure_static.cmd
+++ configure_static.cmd
@@ -7,10 +7,8 @@ set VCPKG_BINARY_SOURCES=clear
 set v="%~dp0vcpkg" install --editable --triplet=%VCPKG_DEFAULT_TRIPLET% --host-triplet=%VCPKG_DEFAULT_TRIPLET% --x-buildtrees-root=b
 %v% pcre icu qt5-base[icu] qt5-script qt5-xmlpatterns
 if %errorlevel% neq 0 exit /b %errorlevel%
-%v% atlmfc qt5-graphicaleffects qt5-location qt5-quickcontrols qt5-quickcontrols2 qt5-serialport qt5-webchannel
-if %errorlevel% neq 0 exit /b %errorlevel%
 %v% protobuf boost xerces-c xalan-c grpc libzip lua[cpp]
-%v% mimalloc[override] sol2 lmdb flatbuffers z3 gmp yices
+%v% mimalloc[override] sol2 lmdb flatbuffers gmp yices
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd "%~dp0installed\%VCPKG_DEFAULT_TRIPLET%"
 rmdir tools\nodejs
--- ports/yices/portfile.cmake
+++ ports/yices/portfile.cmake
@@ -1,15 +1,22 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO isanych/yices2
-    REF efae0c83adda38124849a1e8e3f60dd751e1426a
-    SHA512 1e6b92aa6b708eeae137965fa54b80034054361c4c6e6c240057f10a884a44a8e206e5822f9625ecc5e1930bc5b069c49e0f6b11ff6b393871b6aa6bf411b610
+    REF ceaeab40d7315614b3d71b1d9ea167412fedb2f7
+    SHA512 953945e6c5a6670ca34422e1270581d024f6cb5371f5c160bbcc7215992bf66521b895b3b9796fe77e0e8c7cc7c18c2c937b71476a74c852be31c8e1ca647b3f
     HEAD_REF cmake
 )
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    set(YICES_STATIC ON)
+else()
+    set(YICES_STATIC OFF)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DYICES_CPP=ON
+        -DYICES_STATIC=${YICES_STATIC}
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
