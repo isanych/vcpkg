c5066714cb582ee501bb362e7b4aa0550ed5454d
Igor Kostenko <ikostenko@perforce.com>
2025-03-02 22:03:48 +0000

    extra check

--- configure.cmd
+++ configure.cmd
@@ -5,7 +5,7 @@ if [%VCPKG_QT5%] == [] set VCPKG_QT5=2
 if [%VCPKG_QT6%] == [] set VCPKG_QT6=2
 if [%VCPKG_DEFAULT_TRIPLET%] == [] set VCPKG_DEFAULT_TRIPLET=%x%w
 set VCPKG_DEFAULT_HOST_TRIPLET=%VCPKG_DEFAULT_TRIPLET%
-set VCPKG_FORCE_SYSTEM_BINARIES=1
+rem set VCPKG_FORCE_SYSTEM_BINARIES=1
 if not exist "%~dp0vcpkg.exe" call "%~dp0bootstrap-vcpkg" -disableMetrics
 set VCPKG_BINARY_SOURCES=clear
 set v="%~dp0vcpkg" install --editable --triplet=%VCPKG_DEFAULT_TRIPLET% --host-triplet=%VCPKG_DEFAULT_TRIPLET% --x-buildtrees-root=b
@@ -15,8 +15,10 @@ if %VCPKG_QT6% geq 1 %v% qtbase qtdeclarative qt5compat
 if %errorlevel% neq 0 exit /b %errorlevel%
 if %VCPKG_QT5% geq 2 %v% atlmfc qt5-webengine
 if %errorlevel% neq 0 exit /b %errorlevel%
+if %VCPKG_QT5% geq 2 if not exist installed\%VCPKG_DEFAULT_TRIPLET%\bin\Qt5WebEngineWidgets.dll exit /b 1
 if %VCPKG_QT6% geq 2 %v% qtwebengine
 if %errorlevel% neq 0 exit /b %errorlevel%
+if %VCPKG_QT6% geq 2 if not exist installed\%VCPKG_DEFAULT_TRIPLET%\bin\Qt6WebEngineWidgets.dll exit /b 1
 %v% protobuf boost xerces-c xalan-c grpc libzip lua[cpp]
 if %errorlevel% neq 0 exit /b %errorlevel%
 if %x% == x64 %v% mimalloc[override] sol2 lmdb flatbuffers gmp yices
