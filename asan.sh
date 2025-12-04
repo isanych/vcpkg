c6dfc322f111ab5de7015d2f74ff42311f392e40
Igor Kostenko <ikostenko@perforce.com>
2025-12-04 16:14:11 +0000

    more flexible asan

--- asan.sh
+++ asan.sh
@@ -6,5 +6,14 @@ export VCPKG_QT6=0
 export VCPKG_QT5=0
 unset VCPKG_BASE
 export ASAN_OPTIONS=detect_leaks=0
-export LD_PRELOAD=/lib/x86_64-linux-gnu/libasan.so.8
+if [[ -z "$LD_PRELOAD" ]]; then
+  if [[ -e /usr/lib64/libasan.so.8 ]]; then
+    export LD_PRELOAD=/usr/lib64/libasan.so.8
+  elif [[ -e /lib/x86_64-linux-gnu/libasan.so.8 ]]; then
+    export LD_PRELOAD=/lib/x86_64-linux-gnu/libasan.so.8
+  else
+    echo export "LD_PRELOAD is not set"
+    exit 1
+  fi
+fi
 ./configure.sh
