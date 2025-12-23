4ec16e63a58c046cc5f0659def9e9abd1304080c
Igor Kostenko <ikostenko@perforce.com>
2025-12-23 12:01:02 +0000

    ws only release, better archive

--- archive.cmd
+++ archive.cmd
@@ -1,7 +1,8 @@
 cd "%~dp0.."
 if [%VCPKG_BRANCH%] == [] set VCPKG_BRANCH=2026
-tar czf vcpkg-%VCPKG_BRANCH%-windows.tgz vcpkg/installed/x64w vcpkg/scripts vcpkg/triplets/x64w.cmake vcpkg/.vcpkg-root
-7z a vcpkg-%VCPKG_BRANCH%-windows.7z vcpkg/installed/x64w vcpkg/scripts vcpkg/triplets/x64w.cmake vcpkg/.vcpkg-root
-tar czf vcpkg-%VCPKG_BRANCH%-windows-x64s.tgz vcpkg/installed/x64ws vcpkg/triplets/x64ws.cmake
-tar czf vcpkg-%VCPKG_BRANCH%-windows-x86.tgz vcpkg/installed/x86w vcpkg/triplets/x86w.cmake
-7z a vcpkg-%VCPKG_BRANCH%-windows-src.7z vcpkg/b -xr!x*-venv -xr!x*-rel -xr!x*-dbg -xr!x*-tools
+if exist vcpkg\installed\x64w tar czf vcpkg-%VCPKG_BRANCH%-windows.tgz vcpkg/installed/x64w vcpkg/scripts vcpkg/triplets/x64w.cmake vcpkg/.vcpkg-root
+if exist vcpkg\installed\x64w 7z a vcpkg-%VCPKG_BRANCH%-windows.7z vcpkg/installed/x64w vcpkg/scripts vcpkg/triplets/x64w.cmake vcpkg/.vcpkg-root
+if exist vcpkg\installed\x64ws\debug tar czf vcpkg-%VCPKG_BRANCH%-windows-x64s-debug.tgz vcpkg/installed/x64ws/debug
+if exist vcpkg\installed\x64ws\debug rd /q/s vcpkg\installed\x64ws\debug
+if exist vcpkg\installed\x64ws tar czf vcpkg-%VCPKG_BRANCH%-windows-x64s.tgz vcpkg/installed/x64ws vcpkg/triplets/x64ws.cmake
+if exist vcpkg\installed\x64w 7z a vcpkg-%VCPKG_BRANCH%-windows-src.7z vcpkg/b -xr!x64w*-venv -xr!x64w*-rel -xr!x64w*-dbg -xr!x64w*-tools
