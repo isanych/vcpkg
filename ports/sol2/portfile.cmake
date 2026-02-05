de640ff550e4634c7da25f1aa38468b1031a33a7
Igor Kostenko <ikostenko@perforce.com>
2026-02-05 10:01:55 +0000

    sol2 sync with main

--- ports/sol2/portfile.cmake
+++ ports/sol2/portfile.cmake
@@ -2,9 +2,9 @@ set(VCPKG_BUILD_TYPE release) # header-only
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Nerixyz/sol2
-    REF 2fda4847c7c8f274d13cc98a7d2ea94ef1caab8a
-    SHA512 9d3d91b50f6db7e6ae1538d56a340be1c68cbd516feff8b46816576178f7a0bc19d4f8c25261b97bffa2c1761d1c6ab197ceda3e5c863c74bf5336bb7ddf8e71
+    REPO ThePhD/sol2
+    REF "v${VERSION}"
+    SHA512 5a6ec7e16dae05ad6abea02842f62db8f64935eda438d67b2c264cbee80cee6d82200bd060387c6df837fe9f212dbe22b2772af34df1ce8bd43296dd9429558d
     HEAD_REF develop
     PATCHES
         header-only.patch
